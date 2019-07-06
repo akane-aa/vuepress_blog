b6d47ef627b7e943330f12f707808353d3d8d7b2
akane-aa <kagemiyasenn@gmail.com>
2019-07-07 00:55:44 +0900

    add

---
+++ docs/.vuepress/components/Articles.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <div
+      v-for="page in filteredPages"
+      class="article-container"
+      v-if="!page.frontmatter.exclude"
+    >
+      <h3 class="article-title">
+        <router-link :to="page.path">{{
+          page.title || "No Title"
+        }}</router-link>
+      </h3>
+      <div class="tag-container">
+        <i class="fas fa-tags tag-icon"></i>
+        <div v-for="c in page.frontmatter.category" class="tag">{{ c }}</div>
+      </div>
+      <div v-if="page.frontmatter.description" class="article-description">
+        {{ page.frontmatter.description }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Articles",
+  props: ["pages", "prefix"],
+  computed: {
+    filteredPages() {
+      return this.pages.filter(page => page.path.includes(this.prefix || ""));
+    }
+  }
+};
+</script>
+
+<style scoped lang="stylus">
+.second-line
+  display: flex
+.calendar-icon
+  margin-right: 0.2em;
+.article-container
+  border: 2px solid #e9e9e9;
+  padding: 12px;
+  margin-bottom: 12px;
+  border-radius: 6px;
+.article-title
+  margin: 0;
+  margin-bottom: 6px;
+.tag-container
+  display: flex
+.tag
+  margin-right 0.6em
+  padding 0.2em 0.4em
+  border-radius: 3px;
+  font-size 0.6em
+  cursor: pointer;
+  background-color: #f2fff3;
+.tag-icon
+  margin-left 1em
+  margin-right 0.4em
+.article-description
+  margin-top: 6px;
+</style>
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,16 @@
 module.exports = {
   title: "avelog95",
-  description: "日々のキロク"
+  description: "日々のキロク",
+  plugins: [
+    "@vuepress/blog",
+    {
+      /* options */
+      // Unique ID of current classification
+      id: "post",
+      // Target directory
+      dirname: "_posts",
+      // Path of the `entry page` (or `list page`)
+      path: "/"
+    }
+  ]
 };
